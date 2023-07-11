3f6999e5da52afa6866d560843f2e2c91ebd8404
alejoevilches <alejoevilches@gmail.com>
2023-07-11 16:32:41 -0300

    add handle event for pointermove

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,36 @@ function App() {
   }
 
   useEffect(()=>{
-    console.log("efecto")
-  });
+    const handleMove=(e)=>{
+      const {clientX, clientY}=e;
+      console.log(clientX, clientY)
+    }
+    enabled
+    ? window.addEventListener("pointermove", handleMove)
+    : window.removeEventListener("pointermove", handleMove);
+
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+    };
+
+  },[enabled]);
   return (
     <>
-      <h1>Proyecto 3</h1>
-      <button onClick={handleClick}>{enabled ? "Activar tracking" : "Desactivar tracking"}</button>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(0px, 0px)`
+      }}
+      />
+      <button onClick={handleClick}>{enabled ? "Desactivar tracking" : "Activar tracking"}</button>
     </>
   )
 }
