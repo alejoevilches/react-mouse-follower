5899654d4d32ab0bb2e5f6e0e49e6131a31e0fd3
alejoevilches <alejoevilches@gmail.com>
2023-07-11 16:04:34 -0300

    ready useState feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [enabled, setEnabled]=useState(false);
+
+  const handleClick=()=>{
+    setEnabled(!enabled);
+  }
+
+  useEffect(()=>{
+    console.log("efecto")
+  });
   return (
-    <h1>Proyecto 3</h1>
+    <>
+      <h1>Proyecto 3</h1>
+      <button onClick={handleClick}>{enabled ? "Activar tracking" : "Desactivar tracking"}</button>
+    </>
   )
 }
 
